6c3150f72eeb65bf25db5d75935cced7ba283714
Kosi Engineers <kosiengineers@gmail.com>
2025-03-18 04:41:24 +0530

    Metadata basics, Final Chapter 16 Course Completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
+import { Metadata } from "next";
 import "@/app/ui/global.css"; // Import global styles
 import { inter } from "@/app/ui/fonts"; // Import the Inter font
 import { Analytics } from "@vercel/analytics/react";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Course Dashboard, built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
   children,
--- app/ui/dashboard/invoices/page.tsx
+++ app/ui/dashboard/invoices/page.tsx
@@ -6,6 +6,12 @@ import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
+import { Metadata } from "next";
+
+// Define the metadata for the Invoices page
+export const metadata: Metadata = {
+  title: "Invoices",
+};
 
 export default async function Page(props: {
   // Define the Page component with a searchParams prop
